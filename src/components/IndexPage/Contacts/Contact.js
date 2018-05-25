e99de572ada15449b026b1f18d821fbb819b7b31
Kennerus <kennerus@gmail.com>
2018-05-25 08:44:01 +0300

    # contacts started

---
+++ src/components/IndexPage/Contacts/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, css} from 'aphrodite/no-important';
+
+const Contact = props => {
+  const {text} = props;
+
+  return (
+    <li className={css(styles.li)}>
+      <span className={css(styles.text)}>{text}</span>
+    </li>
+  );
+};
+
+const styles = StyleSheet.create({
+  li: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: '17px',
+  }
+});
+
+Contact.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Contact;
